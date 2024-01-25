4373faae35bacd97231250818ce354ddb6b85f08
cristi <cristi@draghici.net>
2024-01-25 11:20:41 +0200

    add shadcdn as a ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import { useState } from 'react'
+import Button from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="max-w-[800px] p-10">
-      <div className="flex items-center gap-2 p-4 align-middle">
-        <h1>Family tree</h1>
-        <a href="https://cristidraghici.github.com/family-tree" target="_blank">
-          <img src="/tree-16px.png" className="logo" alt="App logo" />
-        </a>
-      </div>
+    <>
+      <nav>
+        <div className="flex gap-4 p-4 align-middle">
+          <h1>Family tree</h1>
+          <a href="https://cristidraghici.github.com/family-tree" target="_blank">
+            <img src="/tree-16px.png" className="logo" alt="App logo" />
+          </a>
+        </div>
+      </nav>
+      <main className="max-w-[800px] p-10">
+        <p className="bg-gray-50 p-4 text-black">
+          Genealogy is a journey, and it often involves uncovering fascinating stories about your
+          ancestors. This application tries to help you enjoy the process! :)
+        </p>
 
-      <p className="bg-gray-50 p-4 text-black">
-        Genealogy is a journey, and it often involves uncovering fascinating stories about your
-        ancestors. This application tries to help you enjoy the process! :)
-      </p>
-
-      <button className="p-4" onClick={() => setCount((count) => count + 1)}>
-        Click count is {count}
-      </button>
-    </div>
+        <Button className="mt-4" onClick={() => setCount((count) => count + 1)}>
+          Click count is {count}
+        </Button>
+      </main>
+    </>
   )
 }
 
