adecd983b1b8ff2683604fc935909de6a2b1f88f
cristi <cristi@draghici.net>
2024-01-29 22:28:34 +0200

    simplify schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -6,56 +6,13 @@ export const personSchema = z.object({
   id: personIdSchema,
 
   firstName: z.string().min(2).max(30),
-  middleName: z.string().min(2).max(30).optional(),
   lastName: z.string().min(2).max(30),
 
   biologicalGender: z.enum(['male', 'female']),
 
-  biography: z.string().min(2).max(1000).optional(),
-
   fatherId: z.string().uuid().optional(),
   motherId: z.string().uuid().optional(),
 
-  details: z
-    .object({
-      previousName: z.string().min(2).max(30).optional(),
-
-      dateOfBirth: z.string().min(2).max(30).optional(),
-      placeOfBirth: z.string().min(2).max(30).optional(),
-
-      dateOfMarriage: z.string().min(2).max(30).optional(),
-      placeOfMarriage: z.string().min(2).max(30).optional(),
-
-      dateOfDeath: z.string().min(2).max(30).optional(),
-      placeOfDeath: z.string().min(2).max(30).optional(),
-      burialPlace: z.string().min(2).max(30).optional(),
-
-      occupation: z.string().min(2).max(30).optional(),
-
-      militaryService: z.string().min(2).max(30).optional(),
-
-      education: z.string().min(2).max(30).optional(),
-
-      residencies: z.string().min(2).max(1000).optional(),
-    })
-    .optional(),
-
-  sources: z
-    .array(
-      z.object({
-        source: z.string().min(2).max(30),
-        details: z.string().min(2).max(1000).optional(),
-        dateOfCollection: z.string().min(2).max(30).optional(),
-      }),
-    )
-    .optional(),
-
-  contacts: z
-    .array(
-      z.object({
-        type: z.enum(['email', 'phone', 'address']),
-        value: z.string().min(2).max(30),
-      }),
-    )
-    .optional(),
+  biography: z.string().min(2).max(1000).optional(),
+  notes: z.string().min(2).max(1000).optional(),
 })
