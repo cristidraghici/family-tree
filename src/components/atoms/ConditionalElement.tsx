c9082dcbf338a51e955a1d27b7b42ebdc092461a
cristi <cristi@draghici.net>
2024-01-29 12:38:02 +0200

    improve the types in the conditional element

--- src/components/atoms/ConditionalElement.tsx
+++ src/components/atoms/ConditionalElement.tsx
@@ -1,30 +1,28 @@
-import { ElementType, ComponentPropsWithoutRef, Fragment, createElement } from 'react'
+import { ReactNode, ElementType, ComponentPropsWithoutRef, Fragment, createElement } from 'react'
 
-// Define a type that includes both HTML elements and custom React components
 type AsElementType = keyof HTMLElementTagNameMap | ElementType
 
-// Define the base properties for the ConditionalElement component
-type ConditionalElementBaseProps<T extends AsElementType> = {
-  as: T | typeof Fragment
-  condition?: boolean
-}
+type ConditionalElementBaseProps<T extends AsElementType> =
+  | ({
+      condition: boolean
+      as: Exclude<T, typeof Fragment>
+    } & ComponentPropsWithoutRef<T>)
+  | {
+      condition: boolean
+      as?: typeof Fragment
+      children: ReactNode
+    }
 
-// Extend the base properties with the properties of the specified element type
-type ConditionalElementProps<T extends AsElementType> = ConditionalElementBaseProps<T> &
-  ComponentPropsWithoutRef<T>
-
-// Define the ConditionalElement component
 const ConditionalElement = <T extends AsElementType>({
   as = Fragment,
-  condition = true,
+  condition,
   children,
   ...props
-}: ConditionalElementProps<T>) => {
-  if (condition === false) {
+}: ConditionalElementBaseProps<T>) => {
+  if (!condition) {
     return null
   }
 
-  // Create the specified element type with the given properties and children
   return createElement(as, props, children)
 }
 
